cea2067eabfda19a6315e0b69250d001240956ac
Rahat Khan Tusar(RKT) <69615463+r3k4t@users.noreply.github.com>
2022-07-08 03:34:52 +0600

    Add files via upload

---
+++ Layer-4/config.js
@@ -0,0 +1,35 @@
+const solanaMetadata = {
+     symbol: "YC", 
+     seller_fee_basis_points: 1000, // Define how much % you want from secondary market sales 1000 = 10% 
+     external_url:"https://github.com/r3k4t", 
+     
+     collection:{
+         name:"Testing",
+         family:"Happy family"
+     }
+     creators: [ 
+     { address: "7fXNuer5sbZtaTEPhtJ5g5gNtuyRoKkvxdjEjEnPN4mC",share: 100, 
+     
+     },
+      ],
+      
+      };
+      
+      
+      
+      const layerConfigurations = [
+       { 
+       growEditionSizeTo: 5,
+       layersOrder: [ 
+       { name: "layer-1",  value:"0"},
+       { name: "layer-2",  value:"0"}, 
+       { name: "layer-3",  value:"1"}, 
+       { name: "layer-4",  value:"0"},
+       {
+            
+            ],
+             },
+             ];
+      
+      
+     
